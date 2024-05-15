ab473af5efaa8901342af95ac3d8e3e22dcf3adb
unknown <unknown@localhost>
2024-05-15 02:59:02 +0200

    Remove unnecessary GL_ERROR

--- src/lut.cpp
+++ src/lut.cpp
@@ -7,45 +7,6 @@
 
 #include "lut.h"
 
-#ifdef NDEBUG
-#define GL_ERROR()
-#else
-#define GL_ERROR()	check_error(__FILE__, __LINE__)
-
-static void check_error(const char* filename, unsigned int line)
-{
-	GLenum error = glGetError();
-	switch(error) {
-		case GL_NO_ERROR:
-			break;
-		case GL_INVALID_ENUM:
-			printf("%s:%u: Error: GL_INVALID_ENUM\n", filename, line);
-			break;
-		case GL_INVALID_VALUE:
-			printf("%s:%u: Error: GL_INVALID_VALUE\n", filename, line);
-			break;
-		case GL_INVALID_OPERATION:
-			printf("%s:%u: Error: GL_INVALID_OPERATION\n", filename, line);
-			break;
-		case GL_INVALID_FRAMEBUFFER_OPERATION:
-			printf("%s:%u: Error: GL_INVALID_FRAMEBUFFER_OPERATION\n", filename, line);
-			break;
-		case GL_OUT_OF_MEMORY:
-			printf("%s:%u: Error: GL_OUT_OF_MEMORY\n", filename, line);
-			exit(1);
-			break;
-		case GL_STACK_UNDERFLOW:
-			printf("%s:%u: Error: GL_STACK_UNDERFLOW\n", filename, line);
-			break;
-		case GL_STACK_OVERFLOW:
-			printf("%s:%u: Error: GL_STACK_OVERFLOW\n", filename, line);
-			break;
-		default:
-			printf("%s:%u: Unknown error 0x%X\n", filename, line, error);
-	}
-}
-#endif
-
 LUT::LUT(const char* filename) : lut(nullptr), points(0), size(0)
 {
 	FILE* f = fopen(filename, "rt");
@@ -172,7 +133,6 @@ GLuint LUT::create_texture()
 	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-	GL_ERROR();
 
 	delete[] data;
 
